99cbc68c8ab38fd0dc8c9c7cf7c0722088e6091f
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-08-17 16:50:43 +0800

    Rename cost to rate

--- src/MobilePhone.java
+++ src/MobilePhone.java
@@ -9,13 +9,13 @@ public class MobilePhone {
     }
 
     public void call(float minutes, char network) {
-        float cost = network == this.network ? this.ratePerCall: this.ratePerCall * 2;
-        this.load -= minutes * cost;
+        float rate = network == this.network ? this.ratePerCall: this.ratePerCall * 2;
+        this.load -= minutes * rate;
     }
 
     public void text(int length, char network) {
-        float cost = network == this.network ? this.ratePerText: this.ratePerText * 2;
-        this.load -= ((float) length / MobilePhone.maxTextSize) * cost;
+        float rate = network == this.network ? this.ratePerText: this.ratePerText * 2;
+        this.load -= ((float) length / MobilePhone.maxTextSize) * rate;
     }
 
     public void reload(float amount) {
