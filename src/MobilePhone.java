d554967f30f17c4838a284c5412e2a3a78a63d20
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-08-17 17:03:33 +0800

    Feat: constructor and helpers

--- src/MobilePhone.java
+++ src/MobilePhone.java
@@ -1,24 +1,56 @@
 public class MobilePhone {
-    private float load = 0;
-    public final char network = 's';
-    public final float ratePerText = 3;
-    public final float ratePerCall = 5;
-    static final int maxTextSize = 160;
+    private float load = 0; //Default
+    public final char network;
+    public final float ratePerText = 1;
+    public final float ratePerCall;
+    public static final int maxTextSize = 160;
+
+    public MobilePhone(float load, char network) {
+        this.load = load;
+        switch(network) {
+            case 'g':
+                this.ratePerCall = 7;
+            break;
+            case 't':
+                this.ratePerCall = 5;
+            break;
+            default:
+                network = 's';
+                this.ratePerCall = 3;
+        }
+        this.network = network;
+    }
+
     public float checkBalance() {
         return this.load;
     }
 
     public void call(float minutes, char network) {
-        float rate = network == this.network ? this.ratePerCall: this.ratePerCall * 2;
-        this.load -= minutes * rate;
+        float rate = this.isSameNetwork(network) ? this.ratePerCall: this.ratePerCall * 2;
+        this.reduce(minutes * rate);
     }
 
     public void text(int length, char network) {
-        float rate = network == this.network ? this.ratePerText: this.ratePerText * 2;
-        this.load -= ((float) length / MobilePhone.maxTextSize) * rate;
+        float rate = this.isSameNetwork(network) ? this.ratePerText: this.ratePerText * 2;
+        float cost = ((float) length / MobilePhone.maxTextSize) * rate;
+        this.reduce(cost);
+    }
+
+    /* @helper */
+    public boolean isSameNetwork(char network) {
+        return this.network == network;
     }
 
     public void reload(float amount) {
-        this.load += amount < 0 ? 0: amount;
+        this.load += amount < 0 ? 0:amount;
+    }
+
+    public void reduce(float amount) throws IllegalArgumentException {
+        if (amount >= 0 && this.load >= amount) {
+            this.load -= amount;
+            return;
+        }
+
+        throw new IllegalArgumentException("Invalid amount or balance");
     }
 }
