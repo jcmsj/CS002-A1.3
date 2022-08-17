dcf22a8f440d8642f5d3393935f4f1d1b6d1ad7c
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-08-17 16:12:49 +0800

    Feat: Assignment 1.1

---
+++ src/MobilePhone.java
@@ -0,0 +1,24 @@
+public class MobilePhone {
+    private float load = 0;
+    public final char network = 's';
+    public final float ratePerText = 3;
+    public final float ratePerCall = 5;
+    static final int maxTextSize = 160;
+    public float checkBalance() {
+        return this.load;
+    }
+
+    public void call(float minutes, char network) {
+        float cost = network == this.network ? this.ratePerCall: this.ratePerCall * 2;
+        this.load -= minutes * cost;
+    }
+
+    public void text(int length, char network) {
+        float cost = network == this.network ? this.ratePerText: this.ratePerText * 2;
+        this.load -= ((float) length / MobilePhone.maxTextSize) * cost;
+    }
+
+    public void reload(float amount) {
+        this.load += amount;
+    }
+}
